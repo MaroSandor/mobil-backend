e26ef87d015a309f7d6ac5a1900b2da54a68f9e3
marosandor <marosandor2001@gmail.com>
2023-01-11 09:08:56 +0100

    1 dolog nem működik

--- backend.js
+++ backend.js
@@ -114,6 +114,22 @@ app.get("/velemenyek", (req, res) => {
   connection.end();
 });
 
+// # Hatodik végpont
+app.get("/velemenyek", (req, res) => {
+  kapcsolat();
+
+  connection.query(
+    "SELECT opinion_id AS route_id, routes.route_short_name, comfort.opinions_desc AS comfort, ido.opinions_desc AS ido, crowd.opinions_desc AS crowd, opinions.opinion_comment FROM opinions INNER JOIN routes ON routes.route_id = opinions.opinion_route INNER JOIN opinions_desc AS comfort ON comfort.opinions_desc_id = opinions.opinion_comfort INNER JOIN opinions_desc AS ido ON ido.opinions_desc_id = opinions.opinion_time INNER JOIN opinions_desc AS crowd ON crowd.opinions_desc_id = opinions.opinion_crowd WHERE routes.route_short_name = '" + req.body.bemenet_1 + "'",
+    (err, rows, fields) => {
+      if (err) throw err;
+
+      console.log("The solution is: ", rows);
+      res.send(rows);
+    }
+  );
+  connection.end();
+});
+
 app.listen(port, () => {
   console.log(
     `Alap backend szerver elérése: https://maro-sandor-peter.dszcbaross.tk`
